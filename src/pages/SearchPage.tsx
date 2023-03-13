a721ddda6632d0314e44d036aa30d89bfa7b8fed
William Pepple <williampepple1@gmail.com>
2023-03-13 21:15:09 +0100

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -172,8 +172,8 @@ const runSearch = () => {
       </header>
 
       <main>
-        {/* Hero section */}
-        <div className="relative isolate overflow-hidden bg-gray-900 pt-14 pb-16 sm:pb-20">
+        {collections.length === 0 &&
+          <div className="relative isolate overflow-hidden bg-gray-900 pt-14 pb-16 sm:pb-20">
           <img
             src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
             alt=""
@@ -316,6 +316,8 @@ const runSearch = () => {
             </svg>
           </div>
         </div>
+        }
+        
 
         <div className='grid grid-cols-3'>
 
